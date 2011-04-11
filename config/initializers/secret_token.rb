fa59aa01197ef16e1e690b3a380c8d95ca7cc4af
Kuba <jakubjapuk@o2.pl>
2011-04-11 16:54:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Screencast::Application.config.secret_token = '65e52ba6cf5ce8172294c18bc0efe05289632748e2103a94238d7070996b54ee36f21f7341b4ef5f1db1a63a16105d774d614e29bc09f62b09587210101ce331'
